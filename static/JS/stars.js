a0963831a5d43dcaeaa6ec44ca5a80de149226f8
Max <maxim.franois@student.uclouvain.be>
2021-12-13 16:30:36 +0100

    changement de l'affichage du nombre d'avis en bas de la page resto.

--- static/JS/stars.js
+++ static/JS/stars.js
@@ -17,3 +17,37 @@ function starNumberUser(numberOfStar){
         }
      };
 }
+
+function noticeByStar(priceStar, meatStar, ambianceStar){
+    var tab = [priceStar, meatStar, ambianceStar];
+    var number1 = 0, number2 = 0, number3 = 0, number4 = 0, number5 = 0;
+    document.getElementsByClassName("bar-1")[0].style.width = "0%";
+    document.getElementsByClassName("bar-2")[0].style.width = "0%";
+    document.getElementsByClassName("bar-3")[0].style.width = "0%";
+    document.getElementsByClassName("bar-4")[0].style.width = "0%";
+    document.getElementsByClassName("bar-5")[0].style.width = "0%";
+    for (var i = 0; i < tab.length; i++){
+        console.log(tab[i])
+        if (tab[i] == 1){
+            number1 += 1;
+        }else if (tab[i] == 2){
+            number2 += 1;
+        }else if (tab[i] == 3){
+            number3 += 1;
+        }else if (tab[i] == 4){
+            number4 += 1;
+        }else if (tab[i] == 5){
+            number5 += 1;
+        }
+    }
+    document.getElementsByClassName("bar-1")[0].style.width = (number1/tab.length)*100+"%";
+    document.getElementsByClassName("bar-2")[0].style.width = (number2/tab.length)*100+"%";
+    document.getElementsByClassName("bar-3")[0].style.width = (number3/tab.length)*100+"%";
+    document.getElementsByClassName("bar-4")[0].style.width = (number4/tab.length)*100+"%";
+    document.getElementsByClassName("bar-5")[0].style.width = (number5/tab.length)*100+"%";
+    document.getElementById("numberBar-1").innerHTML = number1;
+    document.getElementById("numberBar-2").innerHTML = number2;
+    document.getElementById("numberBar-3").innerHTML = number3;
+    document.getElementById("numberBar-4").innerHTML = number4;
+    document.getElementById("numberBar-5").innerHTML = number5;
+}
